8a849874b67cc5b1d521201fd1ab24043577cc00
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-18 06:08:43 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,7 +3,7 @@ def hello_t(array)
     i = 0
 
     while i < array.length
-      yield array[i]
+      array[i]
       i += 1
     end
     array
@@ -13,3 +13,5 @@ def hello_t(array)
 end
 
 # call your method here!
+
+  
